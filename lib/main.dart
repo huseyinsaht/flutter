6572193964eb822895a49e76ecf0a1a2a4b2d747
huseyinsaht <huseyinshtoglu@gmail.com>
2024-08-04 19:52:24 +0200

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,23 @@ void main() {
   runApp(MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   var message = "Ogrenci takip sistemi";
   String seciliOgrenci = "";
-  List<Student> students = [//
-    Student("engin", "demirog",25, "https://media.istockphoto.com/id/1057623146/de/foto/blick-von-der-r%C3%BCckseite-eines-m%C3%A4dchens-in-einem-hut-liegt-auf-einem-h%C3%BCgel-und-befasst-sich-mit.jpg?s=2048x2048&w=is&k=20&c=jThsONVolCVC4MvdMlPdqBehx653RpQfLH9b8q-q-bI="),//
-    Student("as", "aba",100, "https://cdn.pixabay.com/photo/2020/09/27/03/38/woman-5605529_640.jpg"),//
-    Student("kerem", "ava",50, "https://cdn.pixabay.com/photo/2016/01/25/19/48/man-1161337_640.jpg")//
+
+  List<Student> students = [
+    //
+    Student("engin", "demirog", 25,
+        "https://media.istockphoto.com/id/1057623146/de/foto/blick-von-der-r%C3%BCckseite-eines-m%C3%A4dchens-in-einem-hut-liegt-auf-einem-h%C3%BCgel-und-befasst-sich-mit.jpg?s=2048x2048&w=is&k=20&c=jThsONVolCVC4MvdMlPdqBehx653RpQfLH9b8q-q-bI="), //
+    Student("as", "aba", 100,
+        "https://cdn.pixabay.com/photo/2020/09/27/03/38/woman-5605529_640.jpg"), //
+    Student("kerem", "ava", 50,
+        "https://cdn.pixabay.com/photo/2016/01/25/19/48/man-1161337_640.jpg") //
   ];
 
   @override
@@ -28,21 +38,21 @@ class MyApp extends StatelessWidget {
   String sinavhesapla(int not) {
     var sinavSonucu = "";
     if (not > 70) {
-      return sinavSonucu = "gecti";
+      return sinavSonucu = "passed";
     }
-    return sinavSonucu = "kaldi";
+    return sinavSonucu = "failed";
   }
 
   void mesajGoster(BuildContext context, String sinavSonucu) {
     var alert = AlertDialog(
-      title: Text("Sinav sonucu"),
+      title: Text("Note"),
       content: Text(sinavSonucu),
     );
     showDialog(context: context, builder: (BuildContext context) => alert);
   }
 
   Widget buildBody(BuildContext context) {
-   return Column(
+    return Column(
       children: [
         Expanded(
             child: ListView.builder(
@@ -50,49 +60,93 @@ class MyApp extends StatelessWidget {
                 itemBuilder: (BuildContext context, int index) {
                   return ListTile(
                     leading: CircleAvatar(
-                      backgroundImage: NetworkImage(students[index].image)),
-                    title: Text(students[index].firstName + " " + students[index].lastName),
-                    subtitle: Text("Sinavdan aldigi not : " + students[index].grade.toString() + " [" + students[index].getStatus() + "] "),
+                        backgroundImage: NetworkImage(students[index].image)),
+                    title: Text(students[index].firstName +
+                        " " +
+                        students[index].lastName),
+                    subtitle: Text("Note : " +
+                        students[index].grade.toString() +
+                        " [" +
+                        students[index].getStatus() +
+                        "] "),
                     trailing: buildStatusIcon(students[index]),
-                    onTap: (){
-                      seciliOgrenci =students[index].firstName + " " + students[index].lastName;
-                      print(students[index].firstName + " " + students[index].lastName);
+                    onTap: () {
+                      setState(() {
+                        seciliOgrenci = students[index].firstName +
+                            " " +
+                            students[index].lastName;
+                      });
+                      print(students[index].firstName +
+                          " " +
+                          students[index].lastName);
                     },
                   );
                 })),
-        Text("Secili ogrenci : " + seciliOgrenci),
-        Center(
-            child: ElevatedButton(
-                onPressed: () {
-                  var mesaj = sinavhesapla(85);
-                  mesajGoster(context, mesaj);
-                },
-                child: Text("sonucu gor"))),
+        Text("Choosen : " + seciliOgrenci),
+        Row(
+          children: [
+            Flexible(
+                fit: FlexFit.tight,
+                flex: 1,
+                child: ElevatedButton(
+                    onPressed: () {
+                      var mesaj = sinavhesapla(85);
+                      mesajGoster(context, mesaj);
+                    },
+                    child: Row(
+                      children: [
+                        Icon(Icons.add),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text("Add"),
+                      ],
+                    ))),
+            Flexible(
+                fit: FlexFit.tight,
+                flex: 1,
+                child: ElevatedButton(
+                    onPressed: () {
+                      var mesaj = sinavhesapla(85);
+                      mesajGoster(context, mesaj);
+                    },
+                    child: Row(
+                      children: [
+                        Icon(Icons.update),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text("Update"),
+                      ],
+                    ))),
+            Flexible(
+                fit: FlexFit.tight,
+                flex: 1,
+                child: ElevatedButton(
+                    onPressed: () {
+                      var mesaj = sinavhesapla(85);
+                      mesajGoster(context, mesaj);
+                    },
+                    child: Row(
+                      children: [
+                        Icon(Icons.delete),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text("Delete"),
+                      ],
+                    )))
+          ],
+        )
       ],
     );
   }
 
   Widget buildStatusIcon(Student student) {
-     if(student.grade > 50){
-       student.setStatus("Gecti");
-       return  Icon(Icons.done);
-     }
-     student.setStatus("kaldi");
-        return Icon(Icons.clear);
-  }
-}
-
-
-class PersonalManager{
-  void add(){
-    print("Eklendi");
-  }
-
-  void guncelle(){
-    print("Guncellendi");
-  }
-
-  void sil(){
-    print("silindi");
+    student.setStatus(sinavhesapla(student.grade));
+    if (student.grade > 50) {
+      return Icon(Icons.done);
+    }
+    return Icon(Icons.clear);
   }
 }
